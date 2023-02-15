26d85090ba496562735745baf7bbac2b43a4672d
Huy <nguyenviethuy1002@gmail.com>
2023-02-15 13:45:41 +0700

    update

--- clickMenu.js
+++ clickMenu.js
@@ -1,16 +1,17 @@
 const navbar = document.getElementById("navbar")
 const content = document.getElementById("content")
 const MenuIcon = document.querySelector("#menu_icon")
-MenuIcon.addEventListener("click",()=>{
-    if(navbar.classList.contains("d-none")){
-        navbar.classList.remove("d-none")
-        content.classList.add("d-none")
+MenuIcon.onclick = () =>{
+    navbar?.classList.toggle("d-none")
+    content?.classList.toggle("d-none")
+    // if(navbar.classList.contains("d-none")){
+    //     navbar.classList.remove("d-none")
+    //     content.classList.add("d-none")
+    // }
+    // else if (content.classList.contains("d-none")){
+    //     content.classList.remove("d-none")
+    //     navbar.classList.add("d-none")
+    // }
+}
 
-    }
-    else if (content.classList.contains("d-none")){
-        content.classList.remove("d-none")
-        navbar.classList.add("d-none")
-
-    }
-})
-
+// navbar có tồn tại thì navbar.classList.Toggle (d-none)
