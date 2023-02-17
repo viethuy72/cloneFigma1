671cbe25f8dba635eb5f1768d5e5d954b25da74b
Huy <nguyenviethuy1002@gmail.com>
2023-02-17 13:50:04 +0700

    update

--- clickMenu.js
+++ clickMenu.js
@@ -13,3 +13,22 @@ MenuIcon.onclick = () =>{
     //     navbar.classList.add("d-none")
     // }
 }
+const navbar_toggle = document.querySelector("#navbar_toggle")
+    navbar_toggle.onclick = () => {
+    navbar_toggle.classList.toggle("active")
+}
+const navbar_toggle2 = document.querySelector("#navbar_toggle2")
+    navbar_toggle2.onclick = () => {
+    navbar_toggle2.classList.toggle("active")
+}
+
+const navbar_toggle_mobile = document.querySelector("#navbar_toggle-mobile")
+    navbar_toggle_mobile.onclick=()=>{
+    navbar_toggle_mobile.classList.toggle("active")
+}
+const navbar_toggle_mobile2 = document.querySelector("#navbar_toggle2-mobile")
+navbar_toggle_mobile2.onclick=()=>{
+    navbar_toggle_mobile2.classList.toggle("active")
+}
+
+
